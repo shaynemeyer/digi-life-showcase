095cec78abb8d8bfe9c492492b2c587a2228e42c
shaynemeyer <shayne.meyer@gmail.com>
2016-01-02 11:22:29 -0800

    Initial Commit

---
+++ digi-life-showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  digi-life-showcase
+//
+//  Created by Shayne on 1/2/16.
+//  Copyright © 2016 Maynesoft LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
