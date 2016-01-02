2d3b7b83546ac99593933a406ae4f21e816fba39
shaynemeyer <shayne.meyer@gmail.com>
2016-01-02 12:14:37 -0800

    Add TextField, UIView

---
+++ digi-life-showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  digi-life-showcase
+//
+//  Created by Shayne on 1/2/16.
+//  Copyright © 2016 Maynesoft LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
---
+++ digi-life-showcase/MaterialTextField.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialTextField.swift
+//  digi-life-showcase
+//
+//  Created by Shayne on 1/2/16.
+//  Copyright © 2016 Maynesoft LLC. All rights reserved.
+//
+
+import UIKit
+
+class MaterialTextField: UITextField {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
