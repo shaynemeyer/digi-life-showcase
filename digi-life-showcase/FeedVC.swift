0f4a839671c8d70a273923b43db9db1a2ad4a526
shaynemeyer <shayne.meyer@gmail.com>
2016-01-04 13:13:43 -0800

    added downloading data from firebase.

--- digi-life-showcase/FeedVC.swift
+++ digi-life-showcase/FeedVC.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,21 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
             print(snapshot.value)
+            
+            self.posts = []
+            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
+                for snap in snapshots {
+                    print("SNAP: \(snap)")
+                    
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, dictionary: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            
+            self.tableView.reloadData()
         })
     }
 
@@ -28,10 +45,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print(post.postDescription)
+        
         return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
     }
 }
