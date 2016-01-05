02a0f7e46c44cea8a281ee7d5025a80f78274cb5
shaynemeyer <shayne.meyer@gmail.com>
2016-01-04 21:02:43 -0800

    add the ability to add an image.

--- digi-life-showcase/FeedVC.swift
+++ digi-life-showcase/FeedVC.swift
@@ -9,16 +9,27 @@
 import UIKit
 import Firebase
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var postField: MaterialTextField!
     
+    @IBOutlet weak var imageSelectorImage: UIImageView!
     @IBOutlet weak var tableView: UITableView!
     var posts = [Post]()
+    
+    var imagePicker: UIImagePickerController!
+    
+    static var imageCache = NSCache()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        tableView.estimatedRowHeight = 350
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
         
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
             print(snapshot.value)
@@ -51,8 +62,45 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row]
-        print(post.postDescription)
         
-        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell {
+            
+            cell.request?.cancel()
+            
+            var img: UIImage?
+            
+            if let url = post.imageUrl {
+                img = FeedVC.imageCache.objectForKey(url) as? UIImage
+            }
+            cell.configureCell(post,img: img)
+            return cell
+        } else {
+            return PostCell()
+        }
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        let post = posts[indexPath.row]
+        
+        if post.imageUrl == nil {
+            return 150
+        } else {
+            return tableView.estimatedRowHeight
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        imageSelectorImage.image = image 
+    }
+    
+    @IBAction func selectImage(sender: UITapGestureRecognizer) {
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func makePost(sender: AnyObject) {
+        
     }
 }
